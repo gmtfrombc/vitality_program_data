ddbf7962c129e00d4e6488d19dd6b790c90828d7
gmtfrombc <gmtfrombc@gmail.com>
2023-11-06 06:55:22 -0700

    This is just testing stuff

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
+  //this is stuff added as an example to get familiar with github
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
